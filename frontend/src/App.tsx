06265e3cf2d375e8e3cfa3a0cb1e6b7bc526470e
kerloncosta <kerloncosta6@gmail.com>
2026-05-26 14:01:08 -0300

    feat: Importação de tailwindcss e inicio tela de cadastro de usuários

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-
-
 export default function App() {
   return (
-    <div className="App">
-      <h1>Controle de Portaria</h1>
+    <div className="w-full min-h-screen bg-green-950 flex justify-center px-4">
+      <main className="my-10 w-full md:max-w-2xl">
+        <h1 className="text-4xl font-medium text-white">Funcionários</h1>
+
+        
+      </main>
     </div>
   );
 }
